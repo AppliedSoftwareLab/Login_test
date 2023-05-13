8fa131e22812ffeffcdffe3a5d7b118d93b4d6e9
junwon <apro500@naver.com>
2023-05-13 12:44:50 +0900

    Server디렉토리 및 Server.cs파일 추가.

---
+++ 개발중/Server/Server.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Net;
+using System.Net.Sockets;
+using packcl;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+
+namespace Sever
+{
+    class Server
+    {
+        private NetworkStream m_networkstream;
+        private TcpClient m_client;
+        private byte[] sendBuffer = new byte[1024 * 4];
+        private byte[] readBuffer = new byte[1024 * 4];
+        private bool m_bConnect = false;
+        public Login m_loginClass;
+        public G_Play m_gameplay;
+        public Signup m_signup;
+        static void Main(string[] args)
+        {
+        }
+    }
+}
